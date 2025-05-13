541340298e72d6739579e20ce2c8a36e750935d1
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-13 09:29:21 +0500

    Add custom admin configurations for User, Product, and Order models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Product, Order, OrderItem
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'udhaar', 'due_date', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom fields', {'fields': ('role', 'udhaar', 'due_date')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role'),
+        }),
+    )
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_amount', 'status', 'order_date', 'created_at')
+    list_filter = ('status', 'order_date')
+    search_fields = ('user__username', 'user__email')
+    inlines = [OrderItemInline]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'created_at', 'updated_at')
+    list_filter = ('created_at',)
+    search_fields = ('name',)
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
