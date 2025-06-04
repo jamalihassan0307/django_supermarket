50a2076c368afa6c15d211951a63c4b03d3ee1b1
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-06-04 20:13:05 +0500

    Refactor settings.py to improve social authentication integration by adding necessary context processors for social_django and updating STATIC_URL for consistency. Adjust STATICFILES_DIRS to point to the correct static directory.

--- django_supermarket/settings.py
+++ django_supermarket/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    
 ]
 
 MIDDLEWARE = [
@@ -69,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -122,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/static'),
+]
 
 # Media files
 MEDIA_URL = '/media/'
